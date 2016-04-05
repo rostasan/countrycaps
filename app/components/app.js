58ee88c6f4e97be24122917471b5b68c90401f3d
unknown <dickmar@PCNIDICKMAR01.shure.com>
2016-04-05 16:22:00 -0500

    Fixing .gitignore

--- app/components/app.js
+++ app/components/app.js
@@ -1,5 +1,20 @@
 /**
- * Created by 0 on 4/4/2016.
+ * the main module where depencies are declared. []
  */
 angular
-    .module('mainApp',['ngRoute','ngAnimate', 'ngMessages', ngMaterial])
+    .module('mainApp',['ngRoute','ngAnimate', 'ngMessages', 'ngMaterial'])
+    
+//    // These are full file snippets conta<!-- inlining an IIFE
+//    ngapp     // creates an Angular module setter
+//    ngcontroller // creates an Angular controller
+//    ngdirective  // creates an Angular directive
+//    ngfactory    // creates an Angular factory
+//    ngfilter     // creates an Angular filter
+//    ngservice    // creates an Angular service
+//    
+//    // These are partial snippets intended to be chained
+//    ngconfig     // defines a configuration phase function
+//    ngmodule     // creates an Angular module getter
+//    ngroute      // defines an Angular ngRoute 'when' definition
+//    ngrun        // defines a run phase function
+//    ngstate      // creates an Angular UI Router state def -->inition
